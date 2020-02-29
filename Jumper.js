f07329e184a139c2a34b1914a3153e5482ed2f98
Polqw <38992562+Polqw@users.noreply.github.com>
2020-02-28 14:17:56 -1000

    Create Jumper.js

---
+++ Jumper.js
@@ -0,0 +1,42 @@
+
+//use for 'character'
+
+function jumper() {
+  // this.#operation = value;
+  this.x = w/2;
+  this.y = 0;
+  this.gravity = 0.5;//gravity
+  this.lift = -10;//jump height
+  this.velocity = 0;//speed of gravity
+  
+  
+  //function on screen to put values of character will look like
+  this.show = function() {
+    fill(0, 0, 255);
+    ellipse(this.x, this.y, 50,50);
+  };
+  
+  //build function to take intial velocity and change lift/jump
+  this.jump = function() {
+    this.velocity += this.lift;
+  };
+  
+  //builds function to update character
+  this.update = function() {
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+    this.velocity * 0.92; //air resistance
+    
+    //IF character hits groubd, movement in y direction will have jumper from leaving ground
+    if (this.y > h) {
+      this.y = h;
+      this.velocity = 0;
+    }
+    
+    //stops character from leaving ceiling of screen
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
+    }
+  };
+}
