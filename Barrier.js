fd464ad671c4fc6368b29de1be0776af83547a8f
Polqw <38992562+Polqw@users.noreply.github.com>
2020-03-04 13:41:37 -1000

    Create Barrier.js

---
+++ Barrier.js
@@ -0,0 +1,28 @@
+
+//builds a barrier that goes across screen and reappear
+
+function barrier() {
+  this.h = window.innerHeight;
+  this.randh = 0;
+  this.x1 = w-20;
+  this.y1 = 100;
+  this.gravity = 0.5;
+  this.velocity = -10;
+  
+  
+  
+  this.show = function() {
+    fill(255, 0,255);
+    rect(this.x1, this.y1, 20, 200);
+  };
+  
+  this.update = function() {
+    this.x1 += this.velocity;
+    if (this.x1 < 0) {
+      this.y1 = random(h);
+    }
+    //randh = (h - (h * random(h)));
+    
+  };
+
+}
